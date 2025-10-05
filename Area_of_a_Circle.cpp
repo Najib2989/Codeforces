2be68e69e97d16653e349f23a6bc20a62e02376d
Najib2989 <mahmudnajib26@gmail.com>
2025-10-05 21:20:01 +0600

    Add files via upload

---
+++ Area_of_a_Circle.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+     double pi = 3.141592653;
+     double R ;
+     cin >> R ;
+     double Area = pi*pow(R,2);
+     cout << fixed << setprecision(9) << Area << endl;
+     return 0;
+
+}
